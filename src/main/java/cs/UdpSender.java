886f69faf19652cc8a227b31433119470cced9ff
EmilieGreaker1 <emigre01@hotmail.com>
2023-11-17 16:07:17 +0100

    Update UdpSender.java

--- src/main/java/cs/UdpSender.java
+++ src/main/java/cs/UdpSender.java
@@ -3,23 +3,26 @@ package cs;
 import java.io.IOException;
 import java.net.*;
 
-public class UdpBroadcastListener extends Thread{	
-	private String username;
-	byte[] buf = new byte[256];
+public class UdpSender {	
+	private byte[] buf = new byte[256];
 	private DatagramSocket socket;
+	private InetAddress sendToAddress;
+	private int port;
 	
-	public UdpBroadcastListener(String username, int port) throws SocketException { 
-		this.username = username;
+	public UdpSender(byte[] buf, InetAddress sendToAddress, int port) throws SocketException { 
+		this.buf = buf;
+		this.sendToAddress = sendToAddress;
+		this.port = port;
 	    socket = new DatagramSocket(port); 
 	}
 	    
-	@Override
-	public void run() {
-	    while (true) {
-	        DatagramPacket inPacket  = new DatagramPacket(buf, buf.length);
+	public void send() {
+	    /*while (true) {
+	        DatagramPacket packet  = new DatagramPacket(buf, buf.length);        
 	        try {
 	        	System.out.println("Waiting for packet");
 				socket.receive(inPacket);
+				
 				System.out.println("Packet recieved!");
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -27,10 +30,9 @@ public class UdpBroadcastListener extends Thread{
 			}
 	        String received = new String(inPacket.getData(), 0, inPacket.getLength());
 	        System.out.println("Received: " + received);
+	        */
 	        
-	        InetAddress senderAddress = inPacket.getAddress();
-	        int senderPort = inPacket.getPort();
-	        DatagramPacket outPacket = new DatagramPacket(username.getBytes(), username.getBytes().length, senderAddress, senderPort);
+	        DatagramPacket outPacket = new DatagramPacket(buf, buf.length, sendToAddress, port);
 	        try {
 				socket.send(outPacket);
 			} catch (IOException e) {
@@ -38,6 +40,6 @@ public class UdpBroadcastListener extends Thread{
 				e.printStackTrace();
 			}
 	    }
-	}
+	//}
 }
 
