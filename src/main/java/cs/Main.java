2215884537207d4c58438e1b852a8340bbe7b6a9
DanAun <daniel.aunan@gmail.com>
2023-11-18 18:33:44 +0100

    Created test in main

--- src/main/java/cs/Main.java
+++ src/main/java/cs/Main.java
@@ -3,8 +3,15 @@ package cs;
 import java.net.SocketException;
 
 public class Main {
-	public static void main (String[] args) throws SocketException {
-		System.out.println("tester Emilie main class");
-		System.out.print(UdpSender.getAllLocalBroadcastAddresses());
+	public static void main (String[] args) throws SocketException, InterruptedException {
+		System.out.println("Creating contactList object...");
+		ContactList contactList = new ContactList();
+		System.out.println("Created contactList object susccesfully");
+		System.out.println("Running makeContactDict...");
+		contactList.makeContactDict();
+		System.out.println("Sleeping for 5 seconds...");
+		Thread.sleep(5000);
+		System.out.println("Accessing contactDict...");
+		System.out.println(contactList.getContactDict().toString());
 	}
 }
