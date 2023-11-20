d8b49af62168fd06b3ad5eb2a09919d74a9abc97
EmilieGreaker1 <emigre01@hotmail.com>
2023-11-20 15:41:26 +0100

    Update Main.java

--- src/main/java/cs/Main.java
+++ src/main/java/cs/Main.java
@@ -1,30 +1,38 @@
 package cs;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.Scanner;
 
 public class Main {
-	private static ContactList contactList;
-	public static String username;
+	private ContactList contactList;
 	public static void main (String[] args) throws SocketException, InterruptedException {
 	    Scanner input = new Scanner(System.in);  // Create a Scanner object
 	    System.out.print("Enter username: ");
-	    username = input.nextLine();  // Read user input
+	    String username = input.nextLine();  // Read user input
 	    input.close();
-	
-		System.out.println("Creating contactList object from username: " + username + "...");
-		contactList = new ContactList(username);
-		System.out.println("Created contactList object susccesfully");
-		System.out.println("Running makeContactDict...");
+		
+		ContactList contactList = new ContactList(username);
 		contactList.makeContactDict();
-		System.out.println("Sleeping for 5 seconds...");
-		Thread.sleep(5000);
-		System.out.println("Accessing contactDict...");
+		System.out.println("Sleeping for 2 seconds...");
+		Thread.sleep(2000);
+		System.out.println("Online users");
 		System.out.println(contactList.getContactDict().toString());
 		
-		System.out.println("Now online");
-		System.out.println("Listening for other users...");
-		OnJoinHandler onJoinHandlerThread = new OnJoinHandler(contactList);
-		onJoinHandlerThread.setDaemon(false);
-		onJoinHandlerThread.start();
+		
+		
+		UdpListener listener = new UdpListener(8888);
+		listener.start();
+		
+		while(listener.isPacketStackEmpty()) {
+			Thread.sleep(10);
+		}
+		
+		System.out.println("New user joined!\n Updating contactList...");
+		DatagramPacket packet = listener.popPacketStack();
+		String newusername = new String(packet.getData(), 0, packet.getLength());
+		InetAddress ip = packet.getAddress();
+		contactList.getContactDict().put(newusername, ip);
+		System.out.println("New contactList: " + contactList.getContactDict().toString());
 	}
 }
