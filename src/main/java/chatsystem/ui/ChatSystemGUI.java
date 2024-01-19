79675c6f8e0d9e5d6d3d13c5edc0a1f7e5278a69
DanAun <daniel.aunan@gmail.com>
2024-01-19 12:28:41 +0100

    Update ChatSystemGUI.java

--- src/main/java/chatsystem/ui/ChatSystemGUI.java
+++ src/main/java/chatsystem/ui/ChatSystemGUI.java
@@ -182,7 +182,7 @@ public class ChatSystemGUI {
 		List<Contact> contactList = ContactList.getInstance().getAllContacts();
 	
 		// Populate the table model with data from the contactList
-		for (int i = contactList.size(); i >= 0; i--) {
+		for (int i = contactList.size() -1; i >= 0; i--) {
 			String contactName = contactList.get(i).username();
 	
 			// Add a new row to the table model
